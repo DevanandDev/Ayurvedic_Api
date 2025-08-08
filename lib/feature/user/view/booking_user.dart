de01c3d6532c71be34e90250dd563f1a6b61e72a
Devanand <devanandk8078@gmail.com>
2025-08-08 11:58:08 +0530

    updated

---
+++ lib/feature/user/view/booking_user.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MyUserBooking extends StatefulWidget {
+  const MyUserBooking({super.key});
+
+  @override
+  State<MyUserBooking> createState() => _MyUserBookingState();
+}
+
+class _MyUserBookingState extends State<MyUserBooking> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
---
+++ lib/feature/user/view/view_details_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MyUserDetails extends StatelessWidget {
+  const MyUserDetails({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
